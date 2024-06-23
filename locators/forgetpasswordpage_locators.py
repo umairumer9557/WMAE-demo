9da0cb4a741afb8259842c82e6db9ef9c29d27c4
umairumer9557 <umair.umer.9557@gmail.com>
2024-06-23 09:49:13 +0500

    Changings in FPASS locators

--- locators/forgetpasswordpage_locators.py
+++ locators/forgetpasswordpage_locators.py
@@ -4,7 +4,7 @@ class ForgetPasswordPageLocators:
     FORGET_PASSWORD_HEADLINE_TEXT = (By.XPATH, "/html/body/div[2]/div/form/h3[1]")
     EMAIL_FIELD = (By.XPATH, "//input[@id='email']")
     SUBMIT_BUTTON = (By.XPATH, '//button[@data-variant="primary"]')
-    ENTER_OTP_TEXT = (By.XPATH, '//div[@class="text-left"]')
+    ENTER_OTP_TEXTS = (By.XPATH, '//div[@class="text-left"]')
 
 
 
